8429038580da4dbe92f2fb948c81da3478dea563
Trickster-011 <124941463+Trickster-011@users.noreply.github.com>
2024-03-05 16:07:23 -0500

    v0.4
    creando escena del tablero

---
+++ Assets/Scripts/Scene Controler.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneControler : MonoBehaviour
+{
+    public void Exit()
+    {
+        Debug.Log("Salir....");
+        Application.Quit();
+    }
+}
+}
