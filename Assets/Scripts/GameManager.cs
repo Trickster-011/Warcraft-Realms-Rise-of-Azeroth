026d84f9e91dc33dbb677895f4b2132a7c30a791
Trickster-011 <124941463+Trickster-011@users.noreply.github.com>
2024-03-05 00:11:33 -0500

    init
    inciando con scripts iniciales y las cartas

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.PackageManager;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    enum GameState { PREPARED, PLAY, GAMEOVER }
+    private GameState Status;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Status = GameState.PREPARED;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (Status)
+        {
+            case GameState.PREPARED:
+                break;
+            case GameState.PLAY:
+                break;
+            case GameState.GAMEOVER:
+                break;
+        }
+    }
+}
