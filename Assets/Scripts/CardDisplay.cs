811310f9098fb317cfdb5a8f4a7705f2605a833a
Trickster-011 <124941463+Trickster-011@users.noreply.github.com>
2024-03-10 04:29:47 -0400

    0.6

---
+++ Assets/Scripts/CardDisplay.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardDisplay : MonoBehaviour
+{
+
+	public Card card;
+
+	public Text nameText;
+	public Text descriptionText;
+
+	public Image artworkImage;
+
+	public Text tipCardText;
+	public Text attackText;
+
+	// Use this for initialization
+	void Start()
+	{
+		nameText.text = card.name;
+		descriptionText.text = card.description;
+
+		artworkImage.sprite = card.artwork;
+
+		tipCardText.text = card.tipCard.ToString();
+		attackText.text = card.attack.ToString();
+	}
+
+}
